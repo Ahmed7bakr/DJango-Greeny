fca3e49ef189037c9ace39dc17f46d57d467fab8
Ahmed7bakr <a.bakr2472001@gmail.com>
2023-09-18 12:45:12 +0300

    model settings

--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Company (models.Model):
+
+    name = models.CharField(max_length=25)
+    logo = models.ImageField(upload_to='company')
+    call_us = models.CharField(max_length=25)
+    email_us = models.EmailField()
+    subtitle = models.TextField(max_length=500)
+    fb_link = models.URLField(blank=True,null=True)
+    twit_link = models.URLField(blank=True,null=True)
+    insta_link = models.URLField(blank=True,null=True)
+    emails = models.TextField(max_length=100)
+    numbers = models.TextField(max_length=100)
+    address = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
