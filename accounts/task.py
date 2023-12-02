96a423e6baec99c7ba56a387cfbc3b0e6f6887d7
Ahmed7bakr <a.bakr2472001@gmail.com>
2023-12-02 08:08:14 +0200

    redis,celery

--- accounts/task.py
+++ accounts/task.py
@@ -1,1 +1,14 @@
 from project.celery import Celery
+from celery import shared_task
+import time
+
+
+
+
+
+# def print_welcome(n):
+#   time.sleep(n)
+#   print('welcome')
+
+
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+# from .task import print_welcome
+
 
 # Create your views here.
+
+
+
+
+# def test_celery(request):
+#   print_welcome.delay(10)
